1d892c78d80afb8384789e12916a8a17f8c481f3
Sara <mepixop@gmail.com>
2025-02-07 10:14:45 +0100

    add dynamic page text

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.static(path.join(__dirname, "css")));
 app.use(express.static(path.join(__dirname, "js")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/:lang?", (req, res) => {
+  const lang = req.params.lang || "de";
+  const langFile = path.join(__dirname, `public/data/lan_${lang}.json`);
+  let langData = {};
+
+  if (fs.existsSync(langFile)) {
+    langData = JSON.parse(fs.readFileSync(langFile, "utf8"));
+  } else {
+    langData = { error: "Language file not found" };
+  }
+
+  res.render("index", { langData, lang });
 });
 
 app.listen(port, () => {
